9ed6be01abb48da9bae92dabc2b7050938887097
yarik2215 <stikblacklabel@gmail.com>
2020-06-08 23:01:57 +0300

    Add constructors for Rectangle

--- geometry.c
+++ geometry.c
@@ -20,3 +20,35 @@ bool IsPointInRect(const Point* p, const Rectangle* r)
 	return (p->x >= r->left && p->x < r->right && p->y >= r->top && p->y < t-> bottom);
 }
 
+Rectangle RectFromPoints(const Point* leftTop, const Point* rightBottom)
+{
+	Rectangle rect;
+	rect->left_top = leftTop;
+	rect->right_bottom = rightBottom;
+	return rect;
+}
+
+Rectangle RectFromCoord(uint16_t left, uint16_t top, uint16_t right, uint16_t bottom)
+{
+	Rectangle rect;
+	rect->left = left;
+	rect->top = top;
+	rect->right = right;
+	rect->bottom = bottom;
+	return rect;
+}
+
+Rectangle RectFromBounds(uint16_t x, uint16_t y, uint16_t width, uint16_t height)
+{
+	Rectangle rect;
+	rect->left = x;
+	rect->top = y;
+	rect->right = x+width;
+	rect->bottom = y+height;
+	return rect;
+}
+
+bool IsRectEmpty(const Rectangle *r)
+{
+	return (rect->left < rect->rigth && rect->top < rect.bottom);
+}
--- geometry.h
+++ geometry.h
@@ -28,4 +28,9 @@ Point Point(uint16_t x, uint16_t y);
 void PointMove(Point *p, int32_t x, int32_t y);
 bool IsPointInRect(const Point* p, const Rectangle* r);
 
+Rectangle RectFromPoints(const Point* leftTop, const Point* rightBottom);
+Rectangle RectFromCoord(uint16_t left, uint16_t top, uint16_t right, uint16_t bottom);
+Rectangle RectFromBounds(uint16_t x, uint16_t y, uint16_t width, uint16_t height);
+bool IsRectEmpty(const Rectangle *r);
+
 #endif //GEOMETRY_H
